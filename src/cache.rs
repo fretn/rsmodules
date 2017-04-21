bcfe47385e212b2726142b8bfd2e12dbd38b8aa9
Frederik Delaere <frederik.delaere@ugent.be>
2017-04-21 09:45:46 +0200

    bumped version number

--- src/cache.rs
+++ src/cache.rs
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-use std::io::{BufWriter, BufReader, BufRead, Write};
+use std::io::{BufWriter, BufReader, BufRead};
 use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::cmp::Ordering;
