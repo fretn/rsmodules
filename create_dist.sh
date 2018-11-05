e8679e6939c8cd55344d6eec9ff030621296011f
Frederik Delaere <frederik.delaere@ugent.be>
2018-11-05 11:14:38 +0100

    fix build dist script

--- create_dist.sh
+++ create_dist.sh
@@ -4,15 +4,17 @@
 
 VERSION=`cat Cargo.toml  | grep version | awk -F' = ' '{ print $2 }' | sed -e 's/^"//' -e 's/"$//'`
 DIR="rsmodules_$VERSION"
+OS=`uname | tr '[:upper:]' '[:lower:]'`
+MACHINE=`uname -m`
 
 mkdir $DIR 
 cp rsmodules setup_rsmodules.sh setup_rsmodules.csh rsmodules.py rsmodules.pm $DIR
 cp -R examples/ $DIR
 cp -R tools/ $DIR
 cp README.md $DIR
-tar --owner=root --group=root -zcvf "$DIR.tar.gz" $DIR
+tar --owner=root --group=root -zcvf "$DIR""_""$OS""_""$MACHINE.tar.gz" $DIR
 rm -rf "$DIR"
 if [ ! -d "releases" ]; then
 	mkdir releases
 fi
-mv "$DIR.tar.gz" releases/
+mv "$DIR""_""$OS""_""$MACHINE.tar.gz" releases/
