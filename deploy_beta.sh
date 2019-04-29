d7f156ca8d3a17121f756ce3a748b0009d6df5cb
Frederik Delaere <frederik.delaere@ugent.be>
2019-04-29 14:32:50 +0200

    deploy_beta.sh: ask for the hostname when variable doesn't exist

--- deploy_beta.sh
+++ deploy_beta.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 ./build_linux_static.sh
+
+if [ -z ${RSMODULES_DEPLOY_HOST} ]; then
+	echo "The variable RSMODULES_DEPLOY_HOST is not found."
+	echo "Please enter the name of the host you want to deploy to: "
+	read RSMODULES_DEPLOY_HOST
+fi
 scp rsmodules root@$RSMODULES_DEPLOY_HOST:/software/shared/apps/x86_64/rsmodules/beta/rsmodules
 
 
